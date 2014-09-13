7b4aaf014fd717c19a697dd57aeadbd96f2ed754
Ryan S. Brown <sb@ryansb.com>
2014-09-12 23:26:20 -0400

    add stored procedure that calculates knowledge as changes come in

---
+++ gitbyatruck/procedures/calculate_knowledge.sql
@@ -0,0 +1,72 @@
+CREATE OR REPLACE FUNCTION ingest_change() RETURNS TRIGGER AS $new_change$
+    DECLARE
+        adjustment          integer;
+        churn               integer;
+        churn_constant      float;
+        new_knowledge       float;
+    BEGIN
+        --
+        -- Only runs on insert
+        -- TODO: handle UPDATE and DELETE
+        -- result is ignored since this is an AFTER trigger
+        --
+        churn_constant = 0.1;
+        adjustment = NEW.added - NEW.deleted;
+        IF adjustment > 0 THEN
+            -- Add unique knowledge for the contributor
+            UPDATE knol SET knowledge = knowledge + adjustment
+                WHERE committer = NEW.committer
+                AND changed_file = NEW.changed_file
+                AND individual = TRUE;
+            if found THEN
+                -- The update worked, do nothing
+            ELSE
+                -- Update didn't work, insert new node
+                INSERT INTO knol(committer, changed_file, knowledge, individual) VALUES (
+                    NEW.committer,
+                    NEW.changed_file,
+                    adjustment,
+                    TRUE
+                );
+            END IF;
+            UPDATE knol SET knowledge = knowledge + adjustment
+                WHERE changed_file = NEW.changed_file
+                AND individual = FALSE;
+            if found THEN
+            ELSE
+                INSERT INTO knol(changed_file, knowledge, individual) VALUES (
+                    NEW.changed_file,
+                    adjustment,
+                    FALSE
+                );
+            END IF;
+        END IF;
+        IF adjustment < 0 THEN
+            -- Destroy knowledge for developers who have worked on the file by destroying 
+            UPDATE knol SET knowledge = knowledge * (1.0 - adjustment/knowledge)
+                WHERE changed_file = NEW.changed_file;
+        END IF;
+
+
+        -- Now for the fun part, to share knowledge with other developers
+        churn = LEAST(NEW.added, NEW.deleted);
+        if churn = 0 THEN
+            -- There was no churn, so we bail.
+            new_knowledge = churn * churn_constant;
+            UPDATE knol SET knowledge = knowledge - new_knowledge
+                WHERE changed_file = NEW.changed_file
+                AND committer != NEW.committer
+                AND individual = TRUE;
+            UPDATE knol SET knowledge = knowledge + new_knowledge
+                WHERE changed_file = NEW.changed_file
+                AND committer = NEW.committer;
+            RETURN NULL;
+        END IF;
+        RETURN NULL;
+    END;
+$new_change$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER new_change
+AFTER INSERT ON change
+    FOR EACH ROW EXECUTE PROCEDURE ingest_change();
