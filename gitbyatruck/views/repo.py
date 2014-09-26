b7079fa1623832d33f0491d24819e95904068ece
Ryan S. Brown <sb@ryansb.com>
2014-09-26 18:47:34 -0400

    start with new-repo form

--- gitbyatruck/views/repo.py
+++ gitbyatruck/views/repo.py
@@ -2,6 +2,8 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
+import deform
+import colander
 
 from gitbyatruck.models import (
     DBSession,
@@ -9,6 +11,17 @@ from gitbyatruck.models import (
     )
 
 
+class NewRepo(colander.MappingSchema):
+    name = colander.SchemaNode(colander.String())
+    clone_url = colander.SchemaNode(colander.String())
+
+
+form_schema = NewRepo()
+
+
 @view_config(route_name='addrepo', renderer='gitbyatruck:templates/repos.pt')
 def add_repo(request):
-    return {'title': 'RULE', 'one': 'one', 'project': 'gitbyatruck'}
+    new_repo_form = deform.Form(form_schema, buttons=('clone it',))
+    return {'title': 'Repositories',
+            'form': new_repo_form,
+            }
