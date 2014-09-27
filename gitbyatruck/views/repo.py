0ba23ee01e0f6742bec607b62fe8c08be87af223
Ryan S. Brown <sb@ryansb.com>
2014-09-26 23:33:16 -0400

    update background_ingest

--- gitbyatruck/views/repo.py
+++ gitbyatruck/views/repo.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+import logging
+from multiprocessing import Pool
+import json
+
 from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPBadRequest, HTTPAccepted
 
+import transaction
 from sqlalchemy.exc import DBAPIError
 import deform
 import colander
@@ -9,6 +16,12 @@ from gitbyatruck.models import (
     DBSession,
     Repository,
     )
+from gitbyatruck.backend.worker import background_ingest
+
+
+log = logging.getLogger(__name__)
+
+pool = Pool()
 
 
 class NewRepo(colander.MappingSchema):
@@ -25,3 +38,25 @@ def add_repo(request):
     return {'title': 'Repositories',
             'form': new_repo_form,
             }
+
+
+@view_config(route_name='addrepo', renderer='json', request_method='POST')
+def start_repo(request):
+    log.info("Received request {}".format(json.dumps(request.matchdict)))
+
+    if request.json.get('clone_url') is None or request.json.get('name') is None:
+        raise HTTPBadRequest
+    log.info(request.json)
+
+    r = Repository()
+    r.name = request.json['name']
+    r.clone_url = request.json['clone_url']
+    r.created_at = datetime.now()
+    DBSession.add(r)
+    transaction.commit()
+
+    #pool.apply(background_ingest, (request.json['clone_url'],))
+    background_ingest(request.json['clone_url'])
+
+    log.info("Fired async request, done here!")
+    raise HTTPAccepted
