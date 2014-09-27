c408dccb351b594758a2f8b3870ebcd780848bb4
Ryan S. Brown <sb@ryansb.com>
2014-09-26 22:16:44 -0400

    add logging to repo ingestion

--- gitbyatruck/backend/worker.py
+++ gitbyatruck/backend/worker.py
@@ -3,6 +3,7 @@
 # License: Affero GPLv3
 
 from datetime import datetime
+import logging
 import os
 import uuid
 
@@ -11,6 +12,9 @@ import transaction
 
 from gitbyatruck.models import DBSession, Repository
 from gitbyatruck.backend.gen_stats import ingest_worker
+
+
+log = logging.getLogger(__name__)
 
 
 def temp_path(root):
@@ -24,12 +28,19 @@ def temp_path(root):
 
 
 def clone_repo(repo_model, session):
+    log.info("Cloning repo name={name} path={path} clone_url={url}".format(
+        name=repo_model.name,
+        path=repo_model.disk_path,
+        url=repo_model.clone_url)
+    )
     repo = pygit2.clone_repository(
         url=repo_model.clone_url,
         path=repo_model.disk_path,
         bare=True,
     )
+    log.info("Clone successful for {}".format(repo.clone_url))
     ingest_worker(repo, session)
+    log.info("Completed repo {}".format(repo.clone_url))
 
 
 def sanitize_url(orig):
@@ -40,9 +51,12 @@ def sanitize_url(orig):
 def async_ingest(name):
     session = DBSession()
     repo_model = session.query(Repository).filter_by(name=name).first()
+    log.info("Starting repo name {}".format(name))
     fpath = temp_path('/tmp/gitbyatruck-data')
     repo_model.clone_url = sanitize_url(repo_model.clone_url)
     repo_model.disk_path = fpath
+    fpath = temp_path('/tmp/gitbyatruck-data')
+    log.info("Ingesting repo name {}".format(name))
     repo_model.ingest_begun_at = datetime.now()
     transaction.commit()
     clone_repo(repo_model, session)
