086e5ea7402bc8970e6653a0992746aa640e3b8a
Ryan S. Brown <sb@ryansb.com>
2014-10-21 21:30:35 -0400

    pull churn calculation from trigger

--- gitbyatruck/backend/procedures/calculate_knowledge.sql
+++ gitbyatruck/backend/procedures/calculate_knowledge.sql
@@ -2,7 +2,6 @@ CREATE OR REPLACE FUNCTION ingest_change() RETURNS TRIGGER AS $new_change$
     DECLARE
         adjustment          integer;
         churn               integer;
-        churn_constant      float;
         new_knowledge       float;
     BEGIN
         --
@@ -10,7 +9,6 @@ CREATE OR REPLACE FUNCTION ingest_change() RETURNS TRIGGER AS $new_change$
         -- TODO: handle UPDATE and DELETE
         -- result is ignored since this is an AFTER trigger
         --
-        churn_constant = 0.1;
         adjustment = NEW.added - NEW.deleted;
         IF adjustment > 0 THEN
             -- Add unique knowledge for the contributor
@@ -46,23 +44,14 @@ CREATE OR REPLACE FUNCTION ingest_change() RETURNS TRIGGER AS $new_change$
                 AND repo = NEW.repo;
         END IF;
 
-
         -- Now for the fun part, to share knowledge with other developers
-        churn = LEAST(NEW.added, NEW.deleted);
-        if churn = 0 THEN
-            RETURN NULL;
-        END IF;
-        -- There was no churn, so we bail.
-        new_knowledge = churn * churn_constant;
-        UPDATE knol SET knowledge = knowledge - new_knowledge
-            WHERE changed_file = NEW.changed_file
-            AND committer != NEW.committer
-            AND repo = NEW.repo
-            AND individual = TRUE;
-        UPDATE knol SET knowledge = knowledge + new_knowledge
-            WHERE changed_file = NEW.changed_file
-            AND repo = NEW.repo
-            AND committer = NEW.committer;
+        churn_knowledge(
+            NEW.added,
+            NEW.deleted,
+            NEW.changed_file,
+            NEW.repo,
+            NEW.committer)
+
         RETURN NULL;
     END;
 $new_change$ LANGUAGE plpgsql;
