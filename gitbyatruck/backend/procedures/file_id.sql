e29d92a05383bbdb03ab8edff8efb7da470d6b1a
Ryan S. Brown <sb@ryansb.com>
2014-10-22 18:33:53 -0400

    improve performance of file_id function

--- gitbyatruck/backend/procedures/file_id.sql
+++ gitbyatruck/backend/procedures/file_id.sql
@@ -5,22 +5,15 @@ CREATE OR REPLACE FUNCTION maybe_file(
 DECLARE
     fid integer;
 BEGIN
-    -- Now for the fun part, to share knowledge with other developers
-    IF EXISTS(SELECT id FROM files
-        WHERE repo = repo_id
-        AND name = fpath)
-    THEN
-        SELECT id INTO fid FROM files
-        WHERE repo = repo_id
-        AND name = fpath;
-        RETURN fid;
-    ELSE
-        INSERT INTO files (repo, name) VALUES (
-            repo_id,
-            fpath
-        ) RETURNING id INTO fid;
-    END IF;
-    RETURN fid;
+    -- either creates a file or returns the existing ID
+    INSERT INTO files (repo, name) (
+    SELECT repo_id AS repo, fpath AS name
+        WHERE NOT EXISTS (
+            SELECT 1 FROM files
+                WHERE repo = repo_id AND name = fpath
+            )
+    ) RETURNING id INTO fid;
+    return fid;
 END;
 
 $$ LANGUAGE plpgsql;
