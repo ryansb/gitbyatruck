626fb78a37f2c2ae5fa6556cb9ca7966810efba6
Ryan S. Brown <sb@ryansb.com>
2014-10-21 21:57:34 -0400

    fix indentation and return type

--- gitbyatruck/backend/procedures/churn_knowledge.sql
+++ gitbyatruck/backend/procedures/churn_knowledge.sql
@@ -3,33 +3,33 @@ CREATE OR REPLACE FUNCTION churn_knowledge(
     deleted integer,
     changed_fid integer,
     repo integer,
-    committer integer,
-) RETURNS NULL AS $$
-    DECLARE
-        adjustment          integer;
-        churn               integer;
-        churn_constant      float;
-        new_knowledge       float;
-    BEGIN
-        -- Now for the fun part, to share knowledge with other developers
-        churn = LEAST(added, deleted);
-        if churn = 0 THEN
-            RETURN NULL;
-        END IF;
+    committer integer
+) RETURNS void AS $$
+DECLARE
+    adjustment          integer;
+    churn               integer;
+    churn_constant      float;
+    new_knowledge       float;
+BEGIN
+    -- Now for the fun part, to share knowledge with other developers
+    churn = LEAST(added, deleted);
+    if churn = 0 THEN
+        RETURN;
+    END IF;
 
-        churn_constant = 0.1;
+    churn_constant = 0.1;
 
-        -- There was no churn, so we bail.
-        new_knowledge = churn * churn_constant;
-        UPDATE knol SET knowledge = knowledge - new_knowledge
-            WHERE changed_file = changed_fid
-            AND committer != committer
-            AND repo = repo
-            AND individual = TRUE;
-        UPDATE knol SET knowledge = knowledge + new_knowledge
-            WHERE changed_file = changed_fid
-            AND repo = repo
-            AND committer = committer;
-        RETURN NULL;
-    END;
-$new_change$ LANGUAGE plpgsql;
+    -- There was no churn, so we bail.
+    new_knowledge = churn * churn_constant;
+    UPDATE knol SET knowledge = knowledge - new_knowledge
+        WHERE changed_file = changed_fid
+        AND committer != committer
+        AND repo = repo
+        AND individual = TRUE;
+    UPDATE knol SET knowledge = knowledge + new_knowledge
+        WHERE changed_file = changed_fid
+        AND repo = repo
+        AND committer = committer;
+END;
+
+$$ LANGUAGE plpgsql;
